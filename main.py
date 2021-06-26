ee0ef60fcd5efecf3f85db8975eeacf52d639bb6
IvyB <tongao713@live.cn>
2021-06-26 02:01:50 +0100

    finish sketch

--- main.py
+++ main.py
@@ -63,30 +63,42 @@ def tv_order_trend(orderInfo: OrderInfo):
 
     result = swapAPI.get_current_Track('cmt_btcusdt', '1', '100')  # 这里bg有bug，symbol是无效的=-=
     order_to_close = []
-
+    order_size = 0
+    averageOpenPrice = 1000000.0
+    if order_side == 1:   # 当前持有空，接下来开多
+        averageOpenPrice = 0.0
     for position in result:
-        if position["symbol"] == "cmt_btcusdt":
-            print(position["holdSide"])
-            print(position["averageOpenPrice"])
-            print(position["orderNo"])
-            print(position["openTime"])
-            print(position["averageOpenPrice"])
-            if position["holdSide"] != order_side:
-                order_to_close.append(position["orderNo"])
+        if position["symbol"] == "cmt_btcusdt" and position["holdSide"] != order_side:
+            order_to_close.append(position["orderNo"])
+            order_size = max(int(position["openDealCount"]), order_size)
+            if position["holdSide"] == 1 or order_side == 2:
+                averageOpenPrice = min(averageOpenPrice, position["averageOpenPrice"])
+            if position["holdSide"] == 2 or order_side == 1:
+                averageOpenPrice = max(averageOpenPrice, position["averageOpenPrice"])
+
+    if order_side == 1 and (averageOpenPrice-orderInfo.curr_price)/averageOpenPrice > 0.04:
+        order_size = 10
+    elif order_side == 2 and (orderInfo.curr_price-averageOpenPrice)/averageOpenPrice > 0.04:
+        order_size = 10
+    else:
+        order_size += 1
+    if order_size > 20:
+        order_size = 20
+
     for order in order_to_close:
         result = optionAPI.close_track_order('cmt_btcusdt', order)
-        print(result)
+        # print(result)
         time.sleep(1.1)
 
-    if (orderInfo.action == "status" and len(order_to_close)>0) or orderInfo.action == "open":
+    if (orderInfo.action == "status" and len(order_to_close) > 0) or orderInfo.action == "open":
         result = optionAPI.take_order(symbol='cmt_btcusdt', client_oid=str(uuid.uuid4())[0:46], size='10', type=str(order_side),
-                                    order_type='0', match_price='1', price='', presetTakeProfitPrice='', presetStopLossPrice='')
-        print(result)
+                                      order_type='0', match_price='1', price='', presetTakeProfitPrice='', presetStopLossPrice='')
+        # print(result)
 
         time.sleep(1.2)
         result = optionAPI.take_order(symbol='cmt_btcusdt', client_oid=str(uuid.uuid4())[0:46], size='10', type=str(order_side),
-                                    order_type='0', match_price='1', price='', presetTakeProfitPrice='', presetStopLossPrice='')
-        print(result)
+                                      order_type='0', match_price='1', price='', presetTakeProfitPrice='', presetStopLossPrice='')
+        # print(result)
 
     return result
 
